3f0b7c2514206fe541564c2b128f6eaa95e98ad4
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2024-04-13 15:53:47 +1000

    Test updates for package install on slurm

--- scripts/torch_test.py
+++ scripts/torch_test.py
@@ -1,10 +1,13 @@
 import torch
 import numpy as np
 
+from super_resolution.src.config import import_test
 
-x, y, z = torch.zeros(3,3), torch.ones(3,3), torch.rand(3,3)
-print(x,y,z)
-print(x+y)
+print(import_test())
+
+x, y, z = torch.zeros(3, 3), torch.ones(3, 3), torch.rand(3, 3)
+print(x, y, z)
+print(x + y)
 print(y @ z)
 print(z.int())
 print(z.numpy())
@@ -13,4 +16,3 @@ if torch.cuda.is_available():
     print("CUDA is available")
     y, z = y.cuda(), z.cuda()
     print(y @ z)
-
